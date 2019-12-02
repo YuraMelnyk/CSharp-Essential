38c595d6df39b7e2f7664680678b2c4f1b4454de
YuraMelnyk <melnyk.yra27@gmail.com>
2019-12-03 00:54:04 +0200

    Work with simple OOP project.

---
+++ OOP/OOP_Task1/OOP_Task1/Cat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    };
+    public class Cat
+    {
+
+        public string Name { get; }
+        public Gender Gender { get; }
+        private double _energy;
+        public static readonly double MaxEnergy = 20;
+        public static readonly double MinEnergy = 0;
+        public static readonly double SleepEnergyGain = 10;
+        public static readonly double JumpEnergyDrain = 0.5;
+        public Cat(string name, Gender gender)
+        {
+            Name = name;
+            Gender = gender;
+            Energy = Cat.MaxEnergy;
+
+        }
+        public double Energy
+        {
+            get { return _energy; }
+            private set
+            {
+                if (value < Cat.MinEnergy)
+                    throw new Exception("Not enough energy to jump");
+                else
+                    if (value > Cat.MaxEnergy)
+                    _energy = Cat.MaxEnergy;
+                else
+                    _energy = value;
+            }
+        }
+        public void Jump()
+        {
+            Energy -= JumpEnergyDrain;
+        }
+        public void Sleep()
+        {
+            Energy += SleepEnergyGain;
+        }
+
+    }
+}
---
+++ OOP/OOP_Task1/OOP_Task1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var faradayTheCat = new Cat("Faraday", Gender.Male);
+            var adaTheCat = new Cat("Ada", Gender.Female);
+
+            PrintCatInfo(faradayTheCat);
+            PrintCatInfo(adaTheCat);
+
+            faradayTheCat.Jump();
+            PrintCatInfo(faradayTheCat);
+            for (var i = 0; i < 40; i++)
+            {
+                try
+                {
+                    faradayTheCat.Jump();
+                    PrintCatInfo(faradayTheCat);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            PrintCatInfo(adaTheCat);
+            PrintCatInfo(faradayTheCat);
+            for (var i = 0; i < 4; i++)
+            {
+                faradayTheCat.Sleep();
+                PrintCatInfo(faradayTheCat);
+            }
+            PrintCatInfo(adaTheCat);
+            adaTheCat.Sleep();
+            PrintCatInfo(adaTheCat);
+            PrintCatClassInfo();
+            Console.ReadLine();
+        }
+
+        private static void PrintCatInfo(Cat cat)
+        {
+            Console.WriteLine($"{cat.Name}, {cat.Gender}, {cat.Energy}");
+            Console.WriteLine($"{typeof(Cat).GetField("_energy", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(cat)}");
+        }
+
+        private static void PrintCatClassInfo()
+        {
+            var catType = typeof(Cat);
+            var props = catType.GetProperties();
+            var fields = catType.GetFields().Concat(catType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)).ToArray();
+            var constructors = catType.GetConstructors();
+            var methods = catType.GetMethods();
+
+            var sortLambda =
+                new Func<MemberInfo, MemberInfo, int>(
+                    (first, second) =>
+                            String.Compare(first.Name, second.Name, StringComparison.Ordinal));
+            Array.Sort(props, sortLambda.Invoke);
+            Array.Sort(fields, sortLambda.Invoke);
+            Array.Sort(constructors, sortLambda.Invoke);
+            Array.Sort(methods, sortLambda.Invoke);
+            foreach (var propertyInfo in props)
+            {
+                Console.WriteLine(
+                    $"{propertyInfo.Name}, {propertyInfo.PropertyType}, {propertyInfo.CanRead}, {propertyInfo.CanWrite}");
+            }
+            foreach (var fieldInfo in fields)
+            {
+                Console.WriteLine(
+                    $"{fieldInfo.Name}, {fieldInfo.FieldType}, {fieldInfo.IsPrivate}, {fieldInfo.IsPublic}, {fieldInfo.IsStatic}, {fieldInfo.IsInitOnly}, {fieldInfo.IsLiteral}");
+            }
+            foreach (var constructorInfo in constructors)
+            {
+                Console.WriteLine(
+                    $"{constructorInfo.Name}, {constructorInfo.DeclaringType}, {constructorInfo.IsPublic}, {string.Join(", ", constructorInfo.GetParameters().ToArray<object>())}");
+            }
+            foreach (var methodInfo in methods)
+            {
+                Console.WriteLine(
+                    $"{methodInfo.Name}, {methodInfo.DeclaringType}, {methodInfo.IsPublic}, {string.Join(", ", methodInfo.GetParameters().ToArray<object>())}");
+            }
+        }
+    }
+}
