c82ca8b3b075e36d9db06511e7c654f4e90434af
YuraMelnyk <melnyk.yra27@gmail.com>
2020-01-30 15:57:22 +0200

    Shell and Shaker Sort

---
+++ DSA/Sorting/ShakerSort/ShakerSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShakerSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 44, 55, 12, 42, 94, 18, 06, 67 };
+            ShakerSort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        private static void ShakerSort(int[] a)
+        {
+            int l = 0;
+            int r = a.Length;
+            int k = a.Length-1;
+            do
+            {
+                for (int i = r-1; i > l; i--)
+                {
+                    if (a[i-1]>a[i])
+                    {
+                        int temp = a[i - 1];
+                        a[i - 1] = a[i];
+                        a[i] = temp;
+                        k = i;
+                    }
+                }
+                l = k + 1;
+                for (int j = l; j < r; j++)
+                {
+                    if (a[j - 1] > a[j])
+                    {
+                        int temp = a[j - 1];
+                        a[j - 1] = a[j];
+                        a[j] = temp;
+                        k = j;
+                    }
+                }
+                r = k - 1;
+            } while (l > r);
+        }
+    }
+}
---
+++ DSA/Sorting/ShellSort/ShellSort/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 44, 55, 12, 42, 94, 18, 06, 67 };
+            ShellSorting(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        private static void ShellSorting(int[] arr)
+        {
+            for (int i = arr.Length/2; i > 2; i/=2)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    InsSort(arr, j, i);
+                }
+
+            }
+            InsSort(arr, 0, 1);
+        }
+
+        private static void InsSort(int[] arr, int start, int incr)
+        {
+            for (int i = start + incr; i < arr.Length; i+=incr)
+            {
+                for (int j = i; (j >=incr)&&(arr[j]<arr[j-incr]) ; j-=incr)
+                {
+                    Swap(ref arr[j], ref arr[j - incr]);
+                }
+            }
+        }
+
+        private static void ShellSortingExplain(int[] arr)  // it's just example for understand how to work this algorithm
+        {
+            int k = 1;
+            for (int i = 0; i < arr.Length-4; i++)
+            {
+                if (k != 0)
+                {
+                    k = 0;
+                    for (int j = 0; j < arr.Length - 4; j++)
+                    {
+                        if (arr[j] > arr[j + 4])
+                        {
+                            Swap(ref arr[j], ref arr[j + 4]);
+                            ++k;
+                        }
+                    }
+                }
+                else break;
+            }
+
+            k = 1;
+            for (int i = 0; i < arr.Length-2; i++)
+            {
+                if (k != 0)
+                {
+                    k = 0;
+                    for (int j = 0; j < arr.Length - 2; j++)
+                    {
+                        if (arr[j] > arr[j + 2])
+                        {
+                            Swap(ref arr[j], ref arr[j + 2]);
+                            ++k;
+                        }
+                    }
+                }
+                else break;
+            }
+
+            k = 1;
+            for (int i = 0; i < arr.Length-1 ; i++)
+            {
+                if (k != 0)
+                {
+                    k = 0;
+                    for (int j = 0; j < arr.Length - 1; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            Swap(ref arr[j], ref arr[j + 1]);
+                            ++k;
+                        }
+                    } 
+                } 
+                else break;
+            }
+
+        }
+
+        private static void Swap(ref int v1, ref int v2)
+        {
+            int temp = v1;
+            v1 = v2;
+            v2 = temp;
+        }
+    }
+}
