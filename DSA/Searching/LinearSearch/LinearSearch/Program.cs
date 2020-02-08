587956ccbf84bed7874a855b3fac782b5b803f48
YuraMelnyk <melnyk.yra27@gmail.com>
2020-02-08 23:28:31 +0200

    Linear Search

---
+++ DSA/Searching/LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 10, 40 };
+            ShowArray(arr);
+            Console.WriteLine("Enter the search element:");
+            int x = int.Parse(Console.ReadLine().ToString());
+
+            int result = search(arr, x);
+            if (result == -1)
+                Console.WriteLine("Element is not present in array");
+            else
+                Console.WriteLine("Element is present at index " + result);
+            Console.ReadLine();
+        }
+
+        public static int search(int[] arr, int x)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+
+        public static void ShowArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
